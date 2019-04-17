29209021e29e849de0bb4e951c92a0628b3e26f8
Houssem GHIAT <ghiat.houssem@gmail.com>
2019-04-17 20:56:09 +0100

    pp.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 export { ISqlDataMapper } from './sql/common/datamapper';
-export { ISqlOrderByCriteria, ISqlWhereCriteria, SqlOrderByCriteria, SqlWhereCriteria } from './sql/read/criteria';
+export {
+  ISqlOrderByCriteria,
+  ISqlWhereCriteria,
+  SqlOrderByCriteria,
+  SqlWhereCriteria,
+} from './sql/read/criteria';
 
 export { TdsConnectionConfig } from './types';
-export { ITdsDataMapper, TdsDefaultDataMapper } from './tedious/common/datamapper';
-export { TdsGenericQueryHandler, TdsQueryHandler } from './tedious/read/queryhandler';
+export {
+  ITdsDataMapper,
+  TdsDefaultDataMapper,
+} from './tedious/common/datamapper';
+export {
+  TdsGenericQueryHandler,
+  TdsQueryHandler,
+} from './tedious/read/queryhandler';
 export { TdsConnectionPool } from './tedious/connectionpool';
 
 // TODO: implement IQueryHandler.
