20418237cc9b527bf4fad67621c2de9ab445520c
GHIAT houssem <ghiat.houssem@gmail.com>
2019-04-28 14:12:02 +0200

    handle sql queries.

--- src/ioc/inversify/tedious/commandhandler.ts
+++ src/ioc/inversify/tedious/commandhandler.ts
@@ -1,15 +1,15 @@
 import { decorate, inject, injectable } from 'inversify';
 import { Ioc } from 'node-simplecqrs';
 
+import { SqlDeleteCommand, SqlInsertCommand, SqlUpdateCommand } from '../../../sql';
 import { TdsCommandHandler, TdsConnectionConfig } from '../../../tedious';
-import { IocSqlUpdateCommand } from '../../command';
 import { TYPES } from '../constants';
 
 decorate(injectable(), TdsCommandHandler);
 
-@Ioc.commands(IocSqlUpdateCommand)
+@Ioc.commands(SqlDeleteCommand, SqlUpdateCommand, SqlInsertCommand)
 export class InvesrifyTdsCommandHandler extends TdsCommandHandler {
-  constructor(@inject(TYPES.connectionConfig) config: TdsConnectionConfig) {
+  constructor(@inject(TYPES.CONNECTION_CONFIG) config: TdsConnectionConfig) {
     super(config);
   }
 }
--- src/ioc/inversify/tedious/queryhandler.ts
+++ src/ioc/inversify/tedious/queryhandler.ts
@@ -1,18 +1,18 @@
 import { decorate, inject, injectable } from 'inversify';
 import { Ioc } from 'node-simplecqrs';
 
+import { SqlQuery } from '../../../sql';
 import { ITdsDataMapper, TdsConnectionConfig } from '../../../tedious';
 import { TdsQueryHandler } from '../../../tedious/query/queryhandler';
-import { IocSqlQuery } from '../../query';
 import { TYPES } from '../constants';
 
 decorate(injectable(), TdsQueryHandler);
 
-@Ioc.queries(IocSqlQuery)
+@Ioc.queries(SqlQuery)
 export class InversifyTdsQueryHandler<TEntity> extends TdsQueryHandler<TEntity> {
   constructor(
-    @inject(TYPES.connectionConfig) config: TdsConnectionConfig,
-    @inject(TYPES.dataMapper) mapper: ITdsDataMapper<TEntity>
+    @inject(TYPES.CONNECTION_CONFIG) config: TdsConnectionConfig,
+    @inject(TYPES.DATAMAPPER) mapper: ITdsDataMapper<TEntity>
   ) {
     super(config, mapper);
   }
